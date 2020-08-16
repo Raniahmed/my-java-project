90a4289f2cc63ab9bf66d0e7809eb470bab4b47b
Ferdous Nargis <raniferdoussr@gmail.com>
2020-08-16 12:33:24 -0400

    Show the use of variables in java

---
+++ src/variables/Variable.java
@@ -0,0 +1,14 @@
+package variables;
+
+/*
+* Use of variables
+* */
+public class Variable {
+    public static void main(String[] args) {
+        String name = "John Doe";
+        int number = 10;
+
+        System.out.println(name);
+        System.out.println(number);
+    }
+}
