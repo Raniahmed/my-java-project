cb30edd73b82d13eaab2fa631486425f639bd57a
Ferdous Nargis <raniferdoussr@gmail.com>
2020-08-27 15:49:03 -0400

    Show uses of Variables

--- src/variables/Variable.java
+++ src/variables/Variable.java
@@ -5,10 +5,25 @@ package variables;
 * */
 public class Variable {
     public static void main(String[] args) {
-        String name = "John Doe";
-        int number = 10;
 
+        // declare variables
+        int a, b , c;
+
+        // initialize variables
+        a = 10;
+        b = 20;
+        c = 30;
+
+        // declare and initialize variables
+        String name = "John Doe";
+        int number = 15;
+        int value = 100;
+
+        System.out.println(value);
         System.out.println(name);
         System.out.println(number);
+        System.out.println(a);
+        System.out.println(b + "," + c);
+        System.out.println(value + 10);
     }
 }
