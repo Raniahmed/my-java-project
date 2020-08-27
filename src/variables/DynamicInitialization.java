db7e43dbd4b70b4acba2a19dc920cfa4a886bb7e
Ferdous Nargis <raniferdoussr@gmail.com>
2020-08-27 16:42:20 -0400

    Dynamic initialization of Variables

---
+++ src/variables/DynamicInitialization.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class DynamicInitialization {
+
+    public static void main(String[] args) {
+
+        // calculate the area of a rectangle
+        int h = 5;
+        int w = 8;
+
+        //area of a rectangle
+        int a = h * w; //dynamic initialization
+        int A = 50;
+
+        System.out.println(a);
+    }
+}
