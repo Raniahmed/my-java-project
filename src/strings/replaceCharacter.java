a75eb1f57dff5fea4ffc262c0de9af5f1f44071d
Ferdous Nargis <raniferdoussr@gmail.com>
2020-08-29 11:48:25 -0400

    Replace a character in a string

---
+++ src/strings/replaceCharacter.java
@@ -0,0 +1,15 @@
+package strings;
+
+/*
+* Display the total number of words in the myStatement.
+	replace all the 'r' characters with 'f' characters.
+
+* */
+
+public class replaceCharacter {
+    public static void main(String[] args) {
+        String myStatement = "I am a good programmer";
+        System.out.println(myStatement.replace("r","f"));
+
+    }
+}
