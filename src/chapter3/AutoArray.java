84bb5d20a740b424a4b6ec947b8d0b4e9e7fca53
Ferdous Nargis <raniferdoussr@gmail.com>
2020-09-02 22:59:02 -0400

    An improved version of an Array

---
+++ src/chapter3/AutoArray.java
@@ -0,0 +1,12 @@
+package chapter3;
+//An improved version of previous program
+
+public class AutoArray {
+    public static void main(String[] args) {
+
+        int[] month_days = {31,28,30,31,30,31,30,31,31,30,31,30};
+
+        System.out.println("April is " + month_days[3] + " days");
+
+    }
+}
