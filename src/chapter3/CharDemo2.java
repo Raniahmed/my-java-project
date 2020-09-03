0b5b2d417919a8a0a8ea61e7758090550fbc82b8
Ferdous Nargis <raniferdoussr@gmail.com>
2020-09-02 23:10:27 -0400

    Char variable behave like intigers

---
+++ src/chapter3/CharDemo2.java
@@ -0,0 +1,13 @@
+package chapter3;
+
+// Char Variables behave like integers,
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'A'; // 65
+        System.out.println("ch1 contains " + ch1);
+        ch1++; //increment ch1
+        System.out.println("ch1 is now " + ch1);
+    }
+}
