3dcc15dc9c4a34ee5225010cd3780108eb124942
Ferdous Nargis <raniferdoussr@gmail.com>
2020-09-10 17:07:22 -0400

    Modified Resraurent Main

--- src/classes/RestaurantMain.java
+++ src/classes/RestaurantMain.java
@@ -5,15 +5,7 @@ public class RestaurantMain {
         Restaurant restaurant = new Restaurant();
 
         //set Value
-        restaurant.setGuestCapacity(500);
-        restaurant.setGuestCount(450);
 
-        System.out.println("Restaurant Name : " + restaurant.name);
-        System.out.println("Restaurant Guest Capacity : " + restaurant.getGuestCapacity(500));
-        System.out.println("Restaurant guest Count : " + restaurant.getGuestCount(450));
-        System.out.println("Available seats are : " + restaurant.getSeats(500,450));
 
     }
 }
-
-
