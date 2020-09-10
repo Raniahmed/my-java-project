246859a36ef40e23a8336d73b5ccc372a5a15ae6
Ferdous Nargis <raniferdoussr@gmail.com>
2020-09-10 17:10:09 -0400

    Modified Square Main

--- src/classes/SquareMain.java
+++ src/classes/SquareMain.java
@@ -4,14 +4,26 @@ public class SquareMain {
     public static void main(String[] args) {
 
         // create object a Square class
-        Square square = new Square();
+        Square large = new Square(10);
+        Square medium = new Square(8);
+        Square small = new Square(5);
+        Square xs = new Square(3, small);
+
+        System.out.println("Square1 side: " + large.getSide());
+        System.out.println("Area of Square is: " + large.area());
 
         // set values for X
-        square.setSide(4);
-
+        large.setSide(12);
         // call the method
+        System.out.println("Area of Square is " + large.area());
 
-        System.out.println("Area of Square is " + square.area());
+        System.out.println(small.getSide());
+
+        System.out.println(xs.getSide());
+        System.out.println(small.getSide());
+
+        System.out.println(xs.getColor());
+        System.out.println(small.getColor());
 
     }
 }
