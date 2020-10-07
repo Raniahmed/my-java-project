6c604232931df6d8872885b9c93bbf79f4778745
Ferdous Nargis <raniferdoussr@gmail.com>
2020-10-06 21:49:00 -0400

    Print pattern in an Array

---
+++ src/arraysExercise/Exercise3.java
@@ -0,0 +1,25 @@
+package arraysExercise;
+
+/*  - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+*   - - - - - - - - - -
+* */
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        for(int i = 0;i < 10; i ++){
+            for(int j = 0; j < 10; j++) {
+                System.out.print(" - ");
+            }
+            System.out.println( );
+
+        }
+    }
+}
